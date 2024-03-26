be32fe10f922d20435c96d1ab7a13b87c6fb117c
giogina <johanna.langner@arcor.de>
2024-03-27 02:30:46 +0800

    * enabled slider scroll for lhs panel

--- views/spectra_overview.py
+++ views/spectra_overview.py
@@ -1,3 +1,5 @@
+import math
+
 import dearpygui.dearpygui as dpg
 
 from models.experimental_spectrum import ExperimentalSpectrum
@@ -24,8 +26,14 @@ class SpectraOverview:
 
         self.expand_panel_button = self.icons.insert(dpg.add_button(height=20, width=20, pos=(10, 65), show=False, parent="emission tab" if self.viewmodel.is_emission else "excitation tab", callback=lambda s, a, u: self.collapse_spectrum_list(True)), Icons.caret_right, size=16)
 
+        self.disable_ui_update = True
+        self.adjustment_factor = 1
+
         with dpg.handler_registry() as self.mouse_handlers:
             dpg.add_mouse_release_handler(dpg.mvMouseButton_Right, callback=self.on_right_click_release)
+            dpg.add_mouse_wheel_handler(callback=lambda s, a, u: self.on_scroll(a))
+            dpg.add_key_down_handler(dpg.mvKey_Shift, callback=lambda s, a, u: self.toggle_fine_adjustments(u), user_data=True)
+            dpg.add_key_release_handler(dpg.mvKey_Shift, callback=lambda s, a, u: self.toggle_fine_adjustments(u), user_data=False)
 
         with dpg.child_window(width=-1, height=32) as self.spectra_list_action_bar:
 
@@ -73,7 +81,6 @@ class SpectraOverview:
         self.viewmodel.on_x_drag(defaults['xshift'], state_plot.tag, slider=True)
         self.viewmodel.set_y_scale(defaults['yscale'], state_plot)
 
-# todo> enable scroll-sliders here as well
     def add_spectrum(self, state_plot: StatePlot):
         self.spectrum_controls[state_plot.tag] = {}
         with dpg.collapsing_header(label=state_plot.name, parent=self.spectra_list_group, before=self.last_inserted_spec_tag, default_open=True) as self.spectrum_headers[state_plot.tag]:
@@ -101,6 +108,9 @@ class SpectraOverview:
     #     with dpg.collapsing_header(label=exp.name, parent=self.spectra_list_group, default_open=True):  # as self.spectrum_headers[exp.name]:
 
     def update_spectrum(self, state_plot: StatePlot):
+        if self.disable_ui_update:
+            self.disable_ui_update = False
+            return
         if state_plot.tag in self.spectrum_controls.keys() and dpg.does_item_exist(self.spectrum_controls[state_plot.tag].get('hide')):
             dpg.set_value(self.spectrum_controls[state_plot.tag]['xshift'], state_plot.xshift)
             dpg.set_value(self.spectrum_controls[state_plot.tag]['yshift'], state_plot.state.settings.get(f"y shift {state_plot.e_key}", state_plot.yshift))
@@ -134,4 +144,21 @@ class SpectraOverview:
                 dpg.add_theme_style(dpg.mvStyleVar_ItemSpacing, 6, 6)
 
         dpg.bind_item_theme(self.spectra_list_group, spec_list_theme)
+
+    def on_scroll(self, direction):
+        for tag in self.viewmodel.state_plots.keys():
+            for key in ['xshift', 'yscale', 'yshift']:
+                slider = self.spectrum_controls[tag][key]
+                if dpg.does_item_exist(slider) and dpg.is_item_hovered(slider):
+                    step = {'xshift': 10, 'yscale': 0.1, 'yshift': 0.1}[key]
+                    value = dpg.get_value(slider) + step*direction*self.adjustment_factor
+                    dpg.set_value(slider, value)
+                    dpg.get_item_callback(slider)(slider, value, dpg.get_item_user_data(slider))
+                    self.disable_ui_update = True
+
+    def toggle_fine_adjustments(self, fine):
+        if fine:
+            self.adjustment_factor = 0.1
+        else:
+            self.adjustment_factor = 1
 
