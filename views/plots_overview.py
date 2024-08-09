15459af51a8b95d223be9de1950be391bc1453e6
giogina <johanna.langner@arcor.de>
2024-08-09 10:15:38 +0800

    Mulliken sorting functionality

--- views/plots_overview.py
+++ views/plots_overview.py
@@ -193,7 +193,11 @@ class PlotsOverview:
                                         self.label_controls['show labels'] = dpg.add_checkbox(label=" Show Mulliken labels", callback=lambda s, a, u: self.toggle_labels(u), user_data=False, default_value=Labels.settings[self.viewmodel.is_emission].get('show labels', False))
                                         dpg.add_text("   ")
                                         self.label_controls['edit mulliken'] = dpg.add_button(label="Edit", width=50)
-
+                                    with dpg.group(horizontal=False):
+                                        self.label_controls['symmetry order'] = dpg.add_listbox()
+                                        with dpg.group(horizontal=True):
+                                            self.icons.insert(dpg.add_button(height=32, width=32), Icons.caret_up, size=16)
+                                            self.icons.insert(dpg.add_button(height=32, width=32), Icons.caret_down, size=16)
                                     self.label_controls['show gaussian labels'] = dpg.add_checkbox(label=" Show Gaussian labels", callback=lambda s, a, u: self.toggle_labels(u), user_data=True, default_value=Labels.settings[self.viewmodel.is_emission].get('show gaussian labels', False))
                                     self.label_controls['peak intensity label threshold'] = dpg.add_slider_float(label=" Min. Intensity", min_value=0, max_value=0.2, default_value=Labels.settings[self.viewmodel.is_emission].get('peak intensity label threshold', 0.03), callback=lambda s, a, u: Labels.set(self.viewmodel.is_emission, 'peak intensity label threshold', a))
                                     # self.label_controls['peak separation threshold'] = dpg.add_slider_float(label=" Min. Separation", min_value=0, max_value=100, default_value=Labels.settings[self.viewmodel.is_emission].get('peak separation threshold', 1), callback=lambda s, a, u: Labels.set(self.viewmodel.is_emission, 'peak separation threshold', a))  # In original, caused re-draw with higher half-width to smooth out peaks. Probably not necessary.
