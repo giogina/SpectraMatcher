df489bd941257242ce95e3ae8ae1bd40c3e56be5
giogina <johanna.langner@arcor.de>
2025-04-15 14:11:26 +0800

    Check library availability for Drag&Drop setup

--- utility/drop_receiver_window.py
+++ utility/drop_receiver_window.py
@@ -1,33 +1,38 @@
 import dearpygui.dearpygui as dpg
-from pandas.compat import is_platform_windows
 
+try:
+    import DearPyGui_DragAndDrop as dpg_dnd
+    dragndrop_enabled = True
+    DropBaseClass = dpg_dnd.DragAndDrop
+    DropDataObject = dpg_dnd.DragAndDropDataObject
+
+except ImportError:
+    dragndrop_enabled = True
+    # Fallbacks for systems without the library
+    class DummyDpgDnd:
+        def initialize(self, *args, **kwargs): pass
+        def set_drop_effect(self, *args, **kwargs): pass
+        def set_drag_over(self, *args, **kwargs): pass
+        class DragAndDrop:  # Dummy base class
+            def __init__(self, *args, **kwargs): pass
+            def create(self, *args, **kwargs): pass
+
+        class DragAndDropDataObject:
+            def __init__(self, *args, **kwargs): pass
+            def get(self, *args, **kwargs): return None
+            def set(self, *args, **kwargs): pass
+
+    dpg_dnd = DummyDpgDnd()
 
 def initialize_dnd():
-    if is_platform_windows():
-        import DearPyGui_DragAndDrop as dpg_dnd
-        dpg_dnd.initialize()
+    dpg_dnd.initialize()
 
 
 def drag_over(keys):
-    if is_platform_windows():
-        import DearPyGui_DragAndDrop as dpg_dnd
-        dpg_dnd.set_drop_effect()
+    dpg_dnd.set_drop_effect()
 
-if is_platform_windows():
-    import DearPyGui_DragAndDrop as dpg_dnd
-    DropBaseClass = dpg_dnd.DragAndDrop
-    DropDataObject = dpg_dnd.DragAndDropDataObject
-else:
-    class DropBaseClass: # Dummy base class
-        def __init__(self, *args, **kwargs): pass
-        def create(self, *args, **kwargs): pass
 
-    class DropDataObject:
-        def __init__(self, *args, **kwargs): pass
-        def get(self, *args, **kwargs): return None
-        def set(self, *args, **kwargs): pass
-
-class DropReceiverWindow(DropBaseClass):
+class DropReceiverWindow(dpg_dnd.DragAndDrop):
 
     def __init__(self, drop_callback, hover_drag_theme, normal_theme):
         super().__init__()
@@ -38,8 +43,6 @@ class DropReceiverWindow(DropBaseClass):
         self.normal_theme = normal_theme
 
     def create(self, **kwargs):
-        if not is_platform_windows():
-            return
         self.window = dpg.add_child_window(**kwargs)
         dpg.bind_item_theme(self.window, self.normal_theme)
         dpg_dnd.set_drag_over(drag_over)
@@ -47,13 +50,15 @@ class DropReceiverWindow(DropBaseClass):
     def DragOver(self, keyState: list):
         if self.window is None:
             return
+        if not dragndrop_enabled:
+            return
         if dpg.is_item_hovered(self.window):
             dpg.bind_item_theme(self.window, self.hover_drag_theme)
             dpg_dnd.set_drop_effect(dpg_dnd.DROPEFFECT.MOVE)
         else:
             dpg.bind_item_theme(self.window, self.normal_theme)
 
-    def Drop(self, dataObject: DropDataObject, keyState: list):
+    def Drop(self, dataObject: dpg_dnd.DragAndDropDataObject, keyState: list):
         if self.window is None:
             return
         dpg.bind_item_theme(self.window, self.normal_theme)
