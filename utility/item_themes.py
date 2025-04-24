59aa556667d96df54a628fbe3f093ba280df37e7
giogina <johanna.langner@arcor.de>
2025-04-24 12:43:49 +0800

    Singleton-style invisible button theme creation

--- utility/item_themes.py
+++ utility/item_themes.py
@@ -3,18 +3,25 @@ import dearpygui.dearpygui as dpg
 
 class ItemThemes:
 
+    invisible_button_theme = None
+
     @staticmethod
-    def invisible_button_theme():
-        with dpg.theme() as invisible_button_theme:
-            with dpg.theme_component(dpg.mvImageButton):
-                dpg.add_theme_color(dpg.mvThemeCol_Button, [11, 11, 36, 0])
-                dpg.add_theme_color(dpg.mvThemeCol_ButtonHovered, [11, 11, 36, 0])
-                dpg.add_theme_color(dpg.mvThemeCol_ButtonActive, [11, 11, 36, 0])
-            with dpg.theme_component(dpg.mvButton):
-                dpg.add_theme_color(dpg.mvThemeCol_Button, [11, 11, 36, 0])
-                dpg.add_theme_color(dpg.mvThemeCol_ButtonHovered, [11, 11, 36, 0])
-                dpg.add_theme_color(dpg.mvThemeCol_ButtonActive, [11, 11, 36, 0])
-        return invisible_button_theme
+    def get_invisible_button_theme():
+        if ItemThemes.invisible_button_theme is None:
+            try:
+                with dpg.theme() as invisible_button_theme:
+                    with dpg.theme_component(dpg.mvImageButton):
+                        dpg.add_theme_color(dpg.mvThemeCol_Button, [11, 11, 36, 0])
+                        dpg.add_theme_color(dpg.mvThemeCol_ButtonHovered, [11, 11, 36, 0])
+                        dpg.add_theme_color(dpg.mvThemeCol_ButtonActive, [11, 11, 36, 0])
+                    with dpg.theme_component(dpg.mvButton):
+                        dpg.add_theme_color(dpg.mvThemeCol_Button, [11, 11, 36, 0])
+                        dpg.add_theme_color(dpg.mvThemeCol_ButtonHovered, [11, 11, 36, 0])
+                        dpg.add_theme_color(dpg.mvThemeCol_ButtonActive, [11, 11, 36, 0])
+                ItemThemes.invisible_button_theme = invisible_button_theme
+            except Exception as e:
+                print("Error constructing invisible button theme: ", e)
+        return ItemThemes.invisible_button_theme
 
     @staticmethod
     def action_bar_theme():
