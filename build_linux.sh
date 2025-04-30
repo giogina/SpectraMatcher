76af5039c3a2dacad8243603cc97f6b0049ebda5
giogina <johanna.langner@arcor.de>
2025-04-30 15:53:27 +0800

    Linux binary compatible with glibc version 2.31 and newer (using Docker for compilation)

---
+++ build_linux.sh
@@ -0,0 +1,22 @@
+#!/bin/bash
+
+VERSION="1.1.0"
+
+set -e  # Exit on error
+
+echo "=> Building Docker image from Dockerfile..."
+sudo docker build -f linux_installer/Dockerfile -t spectramatcher-builder .
+
+echo "=> Creating temporary container..."
+container_id=$(sudo docker create spectramatcher-builder)
+
+echo "=> Extracting build artifact..."
+sudo docker cp "$container_id:/SpectraMatcher/linux_installer/SpectraMatcher_Linux_Installer.zip" linux_installer/
+mv linux_installer/SpectraMatcher_Linux_Installer.zip "linux_installer/SpectraMatcher_Linux_Installer_$VERSION.zip"
+
+echo "=> Cleaning up..."
+sudo docker rm "$container_id"
+
+echo "Done. Output available at: linux_installer/SpectraMatcher_Linux_Installer.zip"
+
+
